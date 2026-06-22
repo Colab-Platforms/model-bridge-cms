7d4b5c8b32307976831896dd7c7e62a3fcd6e96a
Rohit Saundalkar <soundrohit632@gmail.com>
2026-06-22 18:13:14 +0530

    added user/me route and service

--- backend/src/modules/users/users.controller.ts
+++ backend/src/modules/users/users.controller.ts
@@ -1,1 +1,18 @@
-export {};
+import { Request, Response } from "express";
+
+import STATUS_CODES from "../../utils/statusCodes.js";
+import { sendResponse } from "../../utils/responseUtils.js";
+import { getCurrentUserService } from "./users.service.js";
+
+export const getCurrentUserController = async (req: Request, res: Response) => {
+  const user = (req as Request & { user?: { id?: string } }).user;
+  const result = await getCurrentUserService(user?.id as string);
+
+  return sendResponse(
+    res,
+    true,
+    result,
+    "Current user fetched successfully",
+    STATUS_CODES.OK
+  );
+};
--- backend/src/modules/users/users.route.ts
+++ backend/src/modules/users/users.route.ts
@@ -1,5 +1,9 @@
 import { Router } from "express";
+import auth from "../../shared/middlewares/auth.js";
+import { getCurrentUserController } from "./users.controller.js";
 
 const router = Router();
+
+router.get("/me", auth("USER", "ADMIN", "SUPERADMIN"), getCurrentUserController);
 
 export default router;
